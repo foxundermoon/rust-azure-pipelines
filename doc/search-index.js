4e423c9e53da291ce1fc4d2b5bcf4210bba52a61
Deployment Bot (from Azure Pipelines) <sylwesterrapala@outlook.com>
2019-06-19 22:48:04 +0000

    Deploy API documentation

---
+++ doc/search-index.js
@@ -0,0 +1,5 @@
+var N=null,E="",T="t",U="u",searchIndex={};
+var R=[];
+
+searchIndex["rust_azure_pipelines"]={"doc":"This is just an example project to test templates for…","i":[],"p":[]};
+initSearch(searchIndex);addSearchOptions(searchIndex);
---
+++ doc/source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["rust_azure_pipelines"] = {"name":"","dirs":[],"files":["lib.rs"]};
+createSourceSidebar();
